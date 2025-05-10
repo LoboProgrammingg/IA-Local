8581bb1fb46e486745613d43666b3cc89b1fddcc
loboprogrammingg <matheusloboo2001@gmail.com>
2025-05-10 18:58:17 -0400

    buscando na internet

--- graph.py
+++ graph.py
@@ -4,6 +4,8 @@ from langchain_ollama import ChatOllama
 from langgraph.graph import START, END, StateGraph
 from langgraph.types import Send
 
+from tavily import TavilyClient
+
 from schemas import *
 from prompts import *
 
@@ -15,12 +17,30 @@ reasoning_llm = ChatOllama(model="deepseek-r1:8b")
 
 
 def build_first_queries(state: ReportState):
+    class QueryList(BaseModel):
+        queries: List[str]
     
     user_input = state.user_input
     
     prompt = build_queries.format(user_input=user_input)
+    query_llm = llm.with_structured_output(QueryList)
     
-    return {""}
+    result = query_llm.invoke(prompt)
+    
+    return {"queries": result.queries}
+
+
+def single_search(query: str):
+    query = results.queries[0]
+    tavily_client = TavilyClient()
+    
+    results = tavily_client.search(query,
+                                   max_results=1,
+                                   include_raw_content=False
+                                   )
+    url = results["results"][0]["url"]
+    url_extraction = tavily_client.extract(url)
+
 
 
 builder = StateGraph(ReportState)
